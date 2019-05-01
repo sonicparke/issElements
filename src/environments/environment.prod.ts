7d1619bd1a5a89f5935b4b1b6a9562fa3d9098d4
Brad McAlister <brad.mcalister@vitals.com>
2019-05-01 11:39:35 -0600

    adding keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: ''
+    apiKey: 'AIzaSyBlyu-9J_YsdpO4hZkHajXSz4JoiMSZHF4',
+    authDomain: 'ng-conf-angular-elements-demo.firebaseapp.com',
+    databaseURL: 'https://ng-conf-angular-elements-demo.firebaseio.com',
+    projectId: 'ng-conf-angular-elements-demo'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: ''
+    apiKey: 'AIzaSyBlyu-9J_YsdpO4hZkHajXSz4JoiMSZHF4',
+    authDomain: 'ng-conf-angular-elements-demo.firebaseapp.com',
+    databaseURL: 'https://ng-conf-angular-elements-demo.firebaseio.com',
+    projectId: 'ng-conf-angular-elements-demo'
   }
 };
 
