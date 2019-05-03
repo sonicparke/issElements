c3bf1e2f8feb6526e24813fd31d14e59a6d4d60c
Brad McAlister <brad.mcalister@vitals.com>
2019-05-03 15:10:49 -0600

    removing keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBlyu-9J_YsdpO4hZkHajXSz4JoiMSZHF4',
-    authDomain: 'ng-conf-angular-elements-demo.firebaseapp.com',
-    databaseURL: 'https://ng-conf-angular-elements-demo.firebaseio.com',
-    projectId: 'ng-conf-angular-elements-demo'
+    apiKey: '[YOUR_API_KEY]',
+    authDomain: '[YOUR_AUTH_DOMAIN]',
+    databaseURL: '[YOUR_DB_URL]',
+    projectId: '[YOUR_PROJECT_ID]'
   }
 };
